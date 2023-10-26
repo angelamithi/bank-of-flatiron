a11ed541a078594d383d40c4582dd95e16cde9e4
+ <+>
2023-10-26 18:39:17 +0300

    creation of form components

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "./Button";
 function Form({}){
+    const [formData,setFormData]=useState({
+    date:"",
+    description:"",
+    category:"",
+    amount:"",
+
+    });
     return (
         <div>
             <form>
-            <input  type="date" id="date" value={} />
-            <input type="text" id="description" value={}/>
-            <input  type="text" id="category" value={}/>
-            <input  type="text" id="amount" value={}/>
-            
+            <input  type="date" id="date" placeholder="Date" value={formData.date} />
+            <input type="text" id="description" placeholder="Description" value={formData.description}/>
+            <input  type="text" id="category" placehoceholder="Category" value={formData.category}/>
+            <input  type="text" id="amount" placeholder ="Amount" value={formData.amount}/>
+            <Button/>
             </form>
 
         </div>
